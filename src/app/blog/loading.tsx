726b959754df81320923b5b2a658cc04f4098f66
Shanto93 <shantoislam7363@gmail.com>
2025-08-28 11:51:22 +0600

    Loading UI implemented

---
+++ src/app/blog/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BlogLoading = () => {
+  return (
+    <div>
+      <h2>Blog page Loading.....</h2>
+    </div>
+  );
+};
+
+export default BlogLoading;
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 
-const BlogPage = () => {
+const BlogPage = async () => {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, 2000);
+  });
   return (
     <div>
       <h2>This is Blog Page</h2>
